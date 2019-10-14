1f680ba60a967e2623f903de0822dea9b88a7375
maybenot <594180536@qq.com>
2019-10-14 09:16:31 +0800

    fix_bug

--- src/main/java/third/cloud/service_ribbon/ServiceRibbonApplication.java
+++ src/main/java/third/cloud/service_ribbon/ServiceRibbonApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableHystrix
+@EnableHystrixDashboard
 @EnableFeignClients(basePackages = {"third.cloud.service_ribbon.feignClient"})
 public class ServiceRibbonApplication {
 
